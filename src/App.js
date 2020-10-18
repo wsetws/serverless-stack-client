070440cdae550cea3baa7973572851214677aa0f
root <root@ip-172-31-19-174.sa-east-1.compute.internal>
2020-10-18 04:36:01 +0000

    Setting up our React app -wsetws

--- src/App.js
+++ src/App.js
@@ -1,26 +1,35 @@
-import React from 'react';
-import logo from './logo.svg';
-import './App.css';
+import React from "react";
+import "./App.css";
+import Routes from "./Routes";
+import { Link } from "react-router-dom";
+import { Nav, Navbar, NavItem } from "react-bootstrap";
+import { LinkContainer } from "react-router-bootstrap";
 
 function App() {
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+    <div className="App container">
+      <Navbar fluid collapseOnSelect>
+        <Navbar.Header>
+          <Navbar.Brand>
+            <Link to="/">Scratch</Link>
+          </Navbar.Brand>
+          <Navbar.Toggle />
+        </Navbar.Header>
+        <Navbar.Collapse>
+          <Nav pullRight>
+            <LinkContainer to="/signup">
+		<NavItem href="/signup">Signup</NavItem>
+	    </LinkContainer>
+            <LinkContainer to="login">
+		<NavItem href="/login">Login</NavItem>
+	   </LinkContainer>
+          </Nav>
+        </Navbar.Collapse>
+      </Navbar>
+      <Routes />
     </div>
   );
 }
+
 
 export default App;
---
+++ src/config.js
@@ -0,0 +1,16 @@
+export default {
+  s3: {
+    REGION: "sa-east-1",
+    BUCKET: "notes-app-upload-carlos"
+  },
+  apiGateway: {
+    REGION: "https://z7q7z6x04m.execute-api.sa-east-1.amazonaws.com/prod",
+    URL: "sa-east-1"
+  },
+  cognito: {
+    REGION: "us-east-1",
+    USER_POOL_ID: "us-east-1_Clm0lMEV2",
+    APP_CLIENT_ID: "5dsba3f3j74s77fq8gh25e4pd2",
+    IDENTITY_POOL_ID: "us-east-1:193e9027-57f1-48b2-9705-f8d44b6600c5+"
+  }
+};
