0942fab2bf182c5b187df57a411b6d05af204fc6
chux <wsetws@gmail.com>
2020-10-23 12:26:55 -0300

    Añadidas funcionalidades de crear nota y listar notas

--- src/App.js
+++ src/App.js
@@ -1,34 +1,76 @@
-import React from "react";
+import { onError } from "./libs/errorLib";
+import { useHistory } from "react-router-dom";
+import React, { useState, useEffect } from "react";
 import "./App.css";
 import Routes from "./Routes";
 import { Link } from "react-router-dom";
 import { Nav, Navbar, NavItem } from "react-bootstrap";
 import { LinkContainer } from "react-router-bootstrap";
+import { AppContext } from "./libs/contextLib";
+import { Auth } from "aws-amplify";
 
 function App() {
+  const history = useHistory();
+  const [isAuthenticating, setIsAuthenticating] = useState(true);
+  const [isAuthenticated, userHasAuthenticated] = useState(false);
+
+  useEffect(() => {
+    onLoad();
+  }, []);
+  
+  async function onLoad() {
+    try {
+      await Auth.currentSession();
+      userHasAuthenticated(true);
+    }
+    catch(e) {
+      if (e !== 'No current user') {
+        onError(e);
+      }
+    }
+  
+    setIsAuthenticating(false);
+  }
+  
+  async function handleLogout() {
+    await Auth.signOut();
+  
+    userHasAuthenticated(false);
+    history.push("/login");
+  }
   return (
-    <div className="App container">
-      <Navbar fluid collapseOnSelect>
-        <Navbar.Header>
-          <Navbar.Brand>
-            <Link to="/">Scratch</Link>
-          </Navbar.Brand>
-          <Navbar.Toggle />
-        </Navbar.Header>
-        <Navbar.Collapse>
-          <Nav pullRight>
-            <LinkContainer to="/signup">
-		<NavItem href="/signup">Signup</NavItem>
-	    </LinkContainer>
-            <LinkContainer to="login">
-		<NavItem href="/login">Login</NavItem>
-	   </LinkContainer>
-          </Nav>
-        </Navbar.Collapse>
-      </Navbar>
+  !isAuthenticating &&
+  <div className="App container">
+    <Navbar fluid collapseOnSelect>
+      <Navbar.Header>
+        <Navbar.Brand>
+          <Link to="/">Scratch</Link>
+        </Navbar.Brand>
+        <Navbar.Toggle />
+      </Navbar.Header>
+      <Navbar.Collapse>
+        <Nav pullRight>
+          {isAuthenticated
+            ? <NavItem onClick={handleLogout}>Logout</NavItem>
+            : <>
+                <LinkContainer to="/signup">
+                  <NavItem>Signup</NavItem>
+                </LinkContainer>
+                <LinkContainer to="/login">
+                  <NavItem>Login</NavItem>
+                </LinkContainer>
+              </>
+          }
+        </Nav>
+      </Navbar.Collapse>
+    </Navbar>
+    <AppContext.Provider
+      value={{ isAuthenticated, userHasAuthenticated }}
+    >
       <Routes />
-    </div>
-  );
+    </AppContext.Provider>
+  </div>
+);
 }
 
 
